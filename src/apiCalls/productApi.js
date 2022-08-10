e44e78d85c22536ab58c385499bd7db7bd4bfe41
Zubenna <emelunna@gmail.com>
2022-08-10 12:37:41 +0100

    Disabled user token

--- src/apiCalls/productApi.js
+++ src/apiCalls/productApi.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // const BASE_URL = 'http://localhost:6050/api/';
 const BASE_URL = 'https://homandoff-api.herokuapp.com/api/';
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user);
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root').user));
+
 // .currentUser.token;
 
 export const publicRequest = axios.create({
@@ -12,5 +13,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${TOKEN}` },
+  // headers: { token: `Bearer ${TOKEN}` },
 });
